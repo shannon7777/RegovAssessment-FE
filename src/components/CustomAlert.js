556413e72ed5600c97f52acac8924eb50ff44e96
Shannon Cherry <shannon@Shannons-MacBook-Pro.local>
2023-07-28 16:49:25 +0800

    Included notification component

--- src/components/CustomAlert.js
+++ src/components/CustomAlert.js
@@ -1,8 +1,19 @@
-import { Alert, Snackbar } from "@mui/material";
+import React from "react";
+import { Snackbar } from "@mui/material";
+import MuiAlert from "@mui/material/Alert";
+
+const Alert = React.forwardRef(function Alert(props, ref) {
+  return <MuiAlert elevation={6} ref={ref} variant="filled" {...props} />;
+});
 
 const CustomAlert = ({ message, handleClose, open, variant }) => {
   return (
-    <Snackbar open={open} autoHideDuration={6000} onClose={handleClose}>
+    <Snackbar
+      anchorOrigin={{ vertical: "top", horizontal: "center" }}
+      open={open}
+      autoHideDuration={4000}
+      onClose={handleClose}
+    >
       <Alert onClose={handleClose} severity={variant} sx={{ width: "100%" }}>
         {message}
       </Alert>
